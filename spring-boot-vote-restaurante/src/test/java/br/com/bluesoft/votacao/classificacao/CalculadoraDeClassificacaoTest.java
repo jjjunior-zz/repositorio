649c52a5cd09ac5890ef4f11cfd954456eab9777
jjjunior <jjjunior@gmail.com>
2016-02-21 19:06:07 -0300

    Classes de teste para testes de classificação de restaurante.

---
+++ spring-boot-vote-restaurante/src/test/java/br/com/bluesoft/votacao/classificacao/CalculadoraDeClassificacaoTest.java
@@ -0,0 +1,87 @@
+package br.com.bluesoft.votacao.classificacao;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.bluesoft.votacao.domain.ClassificacaoRestaurante;
+import br.com.bluesoft.votacao.domain.DiferencaClassificacao;
+import br.com.bluesoft.votacao.domain.Restaurante;
+import br.com.bluesoft.votacao.enumeration.RestauranteEnum;
+
+public class CalculadoraDeClassificacaoTest {	
+
+	@Test
+	public void deveCalcularClassificacaoParaRestauranteGanhadorQuandoClassificacaoDeAmbosForemIguais() {
+
+		DiferencaClassificacao dc = DiferencaClassificacao.newIntance(0, 3, 50, 50);
+
+		Restaurante r = Restaurante.newInstance(RestauranteEnum.MCDONALDS.getNome(), RestauranteEnum.MCDONALDS.getPathImagem());
+
+		ClassificacaoRestaurante cr = ClassificacaoRestaurante.newInstance(r,0f);
+
+		CalculadoraDeClassificacao calculadora = new CalculadoraDeClassificacao();
+
+		Classificacao c = new ClassificaGanhador();
+
+		Integer classificacao = calculadora.realizarCalculo(dc, cr, c);
+
+		Assert.assertEquals(5, classificacao.intValue());
+	}
+
+	@Test
+	public void deveCalcularClassificacaoParaRestaurantePerdedorQuandoClassificacaoDeAmbosForemIguais() {
+		
+		DiferencaClassificacao dc = DiferencaClassificacao.newIntance(0, 3, 50, 50);
+
+		Restaurante r = Restaurante.newInstance(RestauranteEnum.MCDONALDS.getNome(), RestauranteEnum.MCDONALDS.getPathImagem());
+
+		ClassificacaoRestaurante cr = ClassificacaoRestaurante.newInstance(r, 0f);
+
+		CalculadoraDeClassificacao calculadora = new CalculadoraDeClassificacao();
+
+		Classificacao c = new ClassificaPerdedor();
+
+		Integer classificacao = calculadora.realizarCalculo(dc, cr, c);
+
+		Assert.assertEquals(-5, classificacao.intValue());
+	}
+	
+	@Test
+	public void deveCalcularClassificacaoParaRestauranteGanhador() {		
+		
+		//Diferenca entre ganhador e perdedor é 100 por isso este registro entre 99 e 106	
+		DiferencaClassificacao dc = DiferencaClassificacao.newIntance(99, 106, 64, 36);
+
+		Restaurante r = Restaurante.newInstance(RestauranteEnum.MCDONALDS.getNome(), RestauranteEnum.MCDONALDS.getPathImagem());
+		
+		ClassificacaoRestaurante cr = ClassificacaoRestaurante.newInstance(r, 100f);
+
+		CalculadoraDeClassificacao calculadora = new CalculadoraDeClassificacao();
+
+		Classificacao c = new ClassificaGanhador();
+
+		Integer classificacao = calculadora.realizarCalculo(dc, cr, c);
+
+		Assert.assertEquals(103, classificacao.intValue());
+	}	
+	
+	@Test
+	public void deveCalcularClassificacaoParaRestaurantePerdedor() {		
+		
+		//Diferenca entre ganhador e perdedor é 100 por isso este registro entre 99 e 106	
+		DiferencaClassificacao dc = DiferencaClassificacao.newIntance(99, 106, 64, 36);
+
+		Restaurante r = Restaurante.newInstance(RestauranteEnum.MCDONALDS.getNome(), RestauranteEnum.MCDONALDS.getPathImagem());
+		
+		//pontuacao anterior 100
+		ClassificacaoRestaurante cr = ClassificacaoRestaurante.newInstance(r, 95f);
+
+		CalculadoraDeClassificacao calculadora = new CalculadoraDeClassificacao();
+
+		Classificacao c = new ClassificaPerdedor();
+
+		Integer classificacao = calculadora.realizarCalculo(dc, cr, c);
+
+		Assert.assertEquals(91, classificacao.intValue());
+	}	
+}
