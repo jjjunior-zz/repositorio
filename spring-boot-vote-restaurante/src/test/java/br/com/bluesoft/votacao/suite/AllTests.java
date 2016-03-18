004698e3b6738bf99e79dad0a30278cc30abe54f
João José Maranhão Junior <jjjunior@gmail.com>
2016-03-18 15:10:51 -0300

    Alteracao de tests

--- spring-boot-vote-restaurante/src/test/java/br/com/bluesoft/votacao/suite/AllTests.java
+++ spring-boot-vote-restaurante/src/test/java/br/com/bluesoft/votacao/suite/AllTests.java
@@ -12,8 +12,13 @@ import br.com.bluesoft.votacao.repository.UsuarioIntegracaoTest;
 import br.com.bluesoft.votacao.util.UtilTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PossivelEscolhaIntegracaoTest.class, RestauranteIntegracaoTest.class, UsuarioIntegracaoTest.class, CalculadoraDeClassificacaoTest.class, ClassificacaoRestauranteIntegracaoTest.class,
-		UtilTest.class })
+@SuiteClasses({ PossivelEscolhaIntegracaoTest.class,
+				RestauranteIntegracaoTest.class,
+				UsuarioIntegracaoTest.class,
+				CalculadoraDeClassificacaoTest.class,
+				ClassificacaoRestauranteIntegracaoTest.class,
+				UtilTest.class
+			 })
 public class AllTests {
 
 }
