382a3ebbb4798dd15c00863f8a34058591a45f04
João José Maranhão Junior <jjjunior@gmail.com>
2016-04-25 10:53:34 -0300

    =problema no frontend

--- cloudws/frontend/app/adapters/application.js
+++
@@ -1,9 +0,0 @@
-import DS from "ember-data";
-
-
-var ApplicationAdapter = DS.RESTAdapter.extend({  
-  host: 'http://localhost:8080',
-  namespace: 'cloudws'  
-});
-
-export default ApplicationAdapter;
